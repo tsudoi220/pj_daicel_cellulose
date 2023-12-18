63f8ec4576465933d0027f18552652b751e0d5a0
TSUDOI YOSHIMOTO <tsudoi@aru-inc.co.jp>
2023-12-18 09:50:50 +0900

    :bookmark: ページ実装

--- src/components/modules/Features/Features.tsx
+++ src/components/modules/Features/Features.tsx
@@ -13,7 +13,7 @@ const Features = ({
   return (
     <section className={"features" + (classNames ? ` ${classNames}` : "")} >
       <div className="features_head">
-        <Heading as="h2" classNames="features_heading _features" data-scroll-animation>セルロースの主な性質</Heading>
+        <Heading as="h3" classNames="features_heading _features" data-scroll-animation>セルロースの主な性質</Heading>
         <Text classNames="features_text _center" data-scroll-animation>植物由来の天然高分子構造を持つセルロースには、さまざまな特徴があります。</Text>
       </div>
       <FeaturesList classNames="features_list" />
--- src/components/modules/StoryHead/StoryHead.tsx
+++ src/components/modules/StoryHead/StoryHead.tsx
@@ -16,7 +16,7 @@ const StoryHead = ({
           <span className="heading_sub"><span className="heading_sub-label">History</span></span>
           <span className="heading_label">ダイセルの< br className="_sp" />セルロース<br />100年の歴史と< br className="_sp" />進化し続ける< br className="_sp" />技術</span>
         </Heading>
-        <Text classNames="storyHead_text _center _wide-s" data-scroll-animation>私たちは創業から< br className="_sp" />100年以上にわたり、< br className="_sp" />セルロースの研究・開発を進め、<br />
+        <Text classNames="storyHead_text _center _wide-s _storyHead" data-scroll-animation>私たちは創業から< br className="_sp" />100年以上にわたり、< br className="_sp" />セルロースの研究・開発を進め、<br />
         セルロースが持つ特長を< br className="_sp" />活かしながら、<br />
         化学修飾して機能化したり、< br className="_sp" />他の素材と組み合わせたりして、<br />
         新しい価値を生み出し続け、< br className="_sp" />世の中に提供しています。</Text>
